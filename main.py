150448d4d8e9d5b701bf9b19e0eccb822d6fc5bc
Eder Basso <eder.sdz@gmail.com>
2023-01-11 15:31:09 -0300

    First DB setup

--- main.py
+++ main.py
@@ -8,7 +8,35 @@ from time import sleep
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+import psycopg2
+from psycopg2 import OperationalError
 
+
+def create_connection():
+    conn = None
+    try:
+        credentials = open('db.credentials', 'r')
+        db_name = credentials.readline().strip('\n\r')
+        db_user = credentials.readline().strip('\n\r')
+        db_password = credentials.readline().strip('\n\r')
+        db_host = credentials.readline().strip('\n\r')
+        db_port = credentials.readline().strip('\n\r')
+        credentials.close()
+
+        conn = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(e)
+    return conn
+
+
+connection = create_connection()
 
 chrome_driver = 'C:/chromedriver/chromedriver.exe'
 chrome_service = Service(chrome_driver)
@@ -44,10 +72,20 @@ for each_item in lista:
     preco = each_item.find_element(By.CLASS_NAME, 'BoxPriceValor').text
     imagem = each_item.find_element(By.CLASS_NAME, 'imagen_buscador').get_attribute('src')
     link = each_item.find_element(By.CLASS_NAME, 'prod_list').get_attribute('href')
-    item = [descricao, preco, imagem, link]
+    item = [id, descricao, preco, imagem, link]
     items.append(item)
     print(id + ' - ' + descricao + ' - ' + preco + ' - ' + link)
 
+for each_record in items:
+    insert_query = """ INSERT INTO sapatilhas_masc (id, descricao, preco, imagem, link) VALUES (%s,%s,%s,%s,%s)"""
+    record = (each_record[0],each_record[1],each_record[2],each_record[3],each_record[4])
+    cursor = connection.cursor()
+    cursor.execute(insert_query, record)
+    connection.commit()
+    count = cursor.rowcount
+    print(count, "Item inserio na tabela")
+    break
 
-
+cursor.close()
+connection.close()
 browser.quit()
